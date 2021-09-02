34ed48f76faa117bbbc2526a2fd79b6fc8b54eaa
YumiMachino <y.y.1515m@gmail.com>
2021-09-01 17:34:43 -0700

    Refactor

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ closeBtn.addEventListener("click", closeSlideMenu);
 function openSlideMenu() {
   document.querySelector("#slide-menu").style.width = "300px";
   document.querySelector("#slide-menu").style.paddingLeft = "60px";
+  closeBtn.classList.remove("animateCloseBtn");
 }
 
 function closeSlideMenu() {
   document.querySelector("#slide-menu").style.width = "0";
   document.querySelector("#slide-menu").style.paddingLeft = "0";
+  closeBtn.classList.add("animateCloseBtn");
 }
